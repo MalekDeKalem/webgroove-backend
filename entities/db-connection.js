80ccae98396e5628b78fc24fad2ab70577fc286b
Viet Pham <s86328@bht-berlin.de>
2024-07-11 22:29:55 +0200

    2nd commit

--- entities/db-connection.js
+++ entities/db-connection.js
@@ -1,11 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 
-const dbHost = process.env.POSTGRES_HOST || 'localhost';
+// const dbHost = process.env.POSTGRES_HOST || 'localhost';
 
-module.exports.sequelize = new Sequelize("postgres", "postgres", "webgroove", {
-    host: dbHost,
+module.exports.sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD , {
+    host: process.env.DB_HOST,
     dialect: "postgres",
-    logging: false,
+    // logging: false,
   });
   
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { sequelize } = require("./entities/db-connection.js");
 const app = require("./src/app.js")
 
-   // localhast starten 
-   
-    const PORT = process.env.PORT || 3999;
-    app.listen(PORT, () => {
-      console.log(`Server läuft auf Port ${PORT}`);
-    });
-  
-// Synchronisieren Sie die Modelle
-sequelize.sync().then(() => {
+// localhast starten 
+
+const PORT = process.env.PORT;
+app.listen(PORT, async () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+
+  // Synchronisieren Sie die Modelle
+try {
+  await sequelize.authenticate();
+  await sequelize.sync();
   console.log('Datenbank & Tabellen erstellt!');
-}).catch(err => {
-  console.log('Fehler beim Erstellen der Datenbank und Tabellen:', err);
+} catch (err) {
+  console.error('Fehler beim Erstellen der Datenbank und Tabellen:', err);
+}
+
 });
 
